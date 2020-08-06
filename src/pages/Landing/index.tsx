1f4dda33d9ddac26a6b1803917b817b6285de385
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-08-05 23:34:13 -0300

    finalizado layout web e mobile pagina home

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import './styles.css';
+import logoImg from "./../../assets/images/logo.svg";
+import landingImg from "./../../assets/images/landing.svg";
+import studyIcon from "./../../assets/images/icons/study.svg";
+import giveClassesIcon from "./../../assets/images/icons/give-classes.svg";
+import purpleHearthIcon from "./../../assets/images/icons/purple-heart.svg";
+
+function Landing() {
+    return (
+        <main id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="Proffy" title="Proffy" />
+                    <h2 className="title">Sua plataforma de estudos online.</h2>
+                </div>
+
+                <img src={landingImg} alt="Plataforma de estudos" title="Plataforma de estudos" />
+
+                <div className="buttons-container">
+                    <a href="/" className="button study">
+                        <img src={studyIcon} alt="Estudar" title="Estudar" /> Estudar
+                    </a>
+                    <a href="/" className="button give-classes">
+                        <img src={giveClassesIcon} alt="Dar aulas" title="Dar aulas" /> Dar aulas
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Total de 150 conexões já realizadas <img src={purpleHearthIcon} alt="Coração roxo" title="Coração roxo" />
+                </span>
+            </div>
+        </main>
+    )
+}
+
+export default Landing;
